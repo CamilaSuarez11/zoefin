5084e4333afb0faa6e14cb247b7b0acb0b6a5c74
camila <muncher@Munchers-MacBook-Air.local>
2022-04-02 14:25:28 -0500

    new test

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# zoefin
-Happy way automation to reschedule an appointment in zoefin
+Reschedule Automation
+Description
+
+This repository is in charge of containing the automation of the test cases of the project to reschedule a meeting in zoefin
+
+requirements
+Test cases will be automated with Selenium WebDriver
+The project must be architecture pom
+Integration with CircleCI will take place
+unnitest libraries will be implemented
+Python sera is the language in which the scripts were created
+
+Authors
+Camila Suarez
---
+++ test/reschedule_test.py
@@ -0,0 +1,30 @@
+import unittest
+from zoefin.src import reschedule_page
+from zoefin.utilities import utilities
+
+
+class TestLogin(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = utilities
+        self.zoefin = reschedule_page
+
+    def test_reschedule(self):
+        self.zoefin.click_date_reschedule()
+        self.assertEqual(self.zoefin.validation_day(), "Wed, Apr 6th 2022")
+        self.zoefin.select_time()
+        self.assertEqual(self.zoefin.validation_time(), "3:00 pm")
+        self.zoefin.click_type_of_call()
+        self.assertEqual(self.zoefin.validation_type_of_call(), "Phone Call")
+        self.zoefin.click_reschedule()
+        self.assertEqual(self.zoefin.validation_pop_up_window(), "Wednesday, Apr 6 2022")
+        self.assertEqual(self.zoefin.validation_hour(), "3:30 PM - 4:30 PM EDT")
+        self.assertEqual(self.zoefin.validation_duration(), "60 Minutes")
+        self.assertEqual(self.zoefin.validation_type(), "Phone")
+        self.zoefin.click_confirm()
+        self.assertEqual(self.zoefin.validation_reschedule_successfully(), "Successfully rescheduled!")
+
+    def tearDown(self):
+        self.driver.close()
+        if __name__ == '__main__':
+            unittest.main()
